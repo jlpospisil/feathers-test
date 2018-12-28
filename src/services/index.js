5ac66e630329010dac07783d99a33824483aaad5
jlpospisil <josh.pospisil@gmail.com>
2018-12-28 11:43:20 -0600

    Added account model and accounts service

--- src/services/index.js
+++ src/services/index.js
@@ -5,7 +5,6 @@ const isDirectory = source => lstatSync(source).isDirectory();
 const getDirectories = source => readdirSync(source).map(name => join(source, name)).filter(isDirectory);
 const serviceNames = getDirectories(__dirname).map(dir => basename(dir));
 
-// eslint-disable-next-line no-unused-vars
 module.exports = function (app) {
   serviceNames.forEach((serviceName) => {
     const service = require(`./${serviceName}/${serviceName}.service.js`);
